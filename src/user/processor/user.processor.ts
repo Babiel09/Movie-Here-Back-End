c1e387f53c1bf042837b671309d4f607a99334d8
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-02 13:47:07 -0300

    feat: create a Worker to process the job

--- src/user/processor/user.processor.ts
+++ src/user/processor/user.processor.ts
@@ -1,8 +1,14 @@
-import { Processor } from "@nestjs/bull";
+import { Process, Processor } from "@nestjs/bull";
 import { USER_QUEUE } from "src/constants/constants";
 import { UserService } from "../user.service";
+import { Job } from "bull";
 
 @Processor(USER_QUEUE)
 export class UserProcessor{
     constructor(private readonly userService:UserService){};
+
+    @Process("login")
+    private async processLogin(job:Job){
+        await this.userService.Login(job.data);
+    };
 };
