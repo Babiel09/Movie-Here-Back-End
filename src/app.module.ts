7b448d7c674c3cce9db47ec9c845777d1c271af3
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-02 11:27:36 -0300

    feat: Incialized the Redis connection with the Bull

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { PismaModule } from 'prisma/prisma.module';
 
 
 @Module({
-  imports: [PismaModule],
+  imports: [
+    PismaModule,
+    BullModule.forRoot({
+      redis:{
+        host:process.env.REDIS_HOST,
+        port:Number(process.env.REDIS_PORT),
+      }
+    }),
+  ],
   controllers: [],
   providers: [],
 })
