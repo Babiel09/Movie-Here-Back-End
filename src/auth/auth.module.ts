d7a21d2a1a569baf30ce5d293377eafe53c45f39
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-02 13:24:27 -0300

    fixed: Tell for the Nest who is my controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from 'src/user/user.module';
+import { AuthController } from './auth.controller';
 
 @Module({
     imports:[UserModule],
-    controllers:[],
+    controllers:[AuthController],
 })
 export class AuthModule {};
