dffebe8a78922fd3ea0eb3e000eb174a61ef722b
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-02 15:51:27 -0300

    config: Configuration of the JWT is done.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-    imports:[UserModule],
-    controllers:[AuthController],
+    imports: [
+        UserModule,
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+            signOptions: {
+                expiresIn: "30 days"
+            }
+        }),
+    ],
+    controllers: [AuthController],
 })
-export class AuthModule {};
+export class AuthModule { };
