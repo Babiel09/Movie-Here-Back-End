473bf61a2834a8ea91576be0a6817fd3baf4d5f3
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-02 13:33:27 -0300

    feat: Created a login method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,40 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Logger, Post, Res } from "@nestjs/common";
+import { Response } from "express";
 import { UserService } from "src/user/user.service";
+import * as bcrypt from "bcrypt";
 
 @Controller("auth")
 export class AuthController{
+    private readonly logger = new Logger(AuthController.name);
     constructor(private readonly userService:UserService){};
+
+    @Post("/v2/login")
+    private async login(@Res()res:Response, @Body()data:{email:string,password:string}):Promise<Response>{
+        try{
+            if(!data.email){
+                this.logger.error(`You need to pass the email!`);
+                return res.status(400).json({server:`You need to pass the email!`});
+            };
+
+            if(!data.password){
+                this.logger.error(`You need to pass the password!`);
+                return res.status(400).json({server:`You need to pass the password!`});
+            };
+
+            const findUserByEmail = await this.userService.Login(data.email);
+
+            const verifyPassword = await bcrypt.compare(data.email,findUserByEmail.password);
+
+            if(verifyPassword === false){
+                this.logger.error(`The password doesn't matc!`);
+                return res.status(401).json({server:`Invalid credentials!`});
+            };
+
+            return res.status(202).send(findUserByEmail);
+
+        } catch(err){
+            this.logger.error(`${err.message}`);
+            return res.status(500).json({server:`${err.message}`});
+        };
+    };
 };
