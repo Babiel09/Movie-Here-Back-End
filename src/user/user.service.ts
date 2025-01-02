da9653341726c60842b83472785210bb687b9be8
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-02 11:41:45 -0300

    feat: User Service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { Prisma } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
+import { PrismaService } from "prisma/prisma.service";
 
 @Injectable()
-export class UserService{};
+export class UserService{
+    private readonly prisma: Prisma.UserDelegate<DefaultArgs>;
+    constructor(private readonly pr:PrismaService){
+        this.prisma = pr.user;
+    };
+};
