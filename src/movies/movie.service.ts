b89acc937a926a5ade07ce612a13b3624e563afb
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-03 20:18:49 -0300

    feat: New method to get all the movies up to 1900

--- src/movies/movie.service.ts
+++ src/movies/movie.service.ts
@@ -10,9 +10,21 @@ export class MovieService{
         const headers = {
             Authorization: `Bearer ${process.env.TMDB_TOKEN}`,
         };
-        
+
         return lastValueFrom(
             this.httpService.get("https://api.themoviedb.org/3/authentication",{
+                headers
+            })
+        );
+    };
+
+    public async getAllMovies(){
+        const headers = {
+            Authorization: `Bearer ${process.env.TMDB_TOKEN}`,
+        };
+
+        return lastValueFrom(
+            this.httpService.get("https://api.themoviedb.org/3/movie/changes?end_date=2025-01-01&page=1&start_date=1900-01-01",{
                 headers
             })
         );
