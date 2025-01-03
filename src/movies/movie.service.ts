fb0990c56bb2f6177fc2a29902c4c0176842d1af
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-03 19:57:56 -0300

    feat: Consuming the movie API

--- src/movies/movie.service.ts
+++ src/movies/movie.service.ts
@@ -1,4 +1,20 @@
+import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
-export class MovieService{};
+export class MovieService{
+    constructor(private readonly httpService:HttpService){};
+    
+    public async movieAPITest(){
+        const headers = {
+            Authorization: `Bearer ${process.env.TMDB_TOKEN}`,
+        };
+        
+        return lastValueFrom(
+            this.httpService.get("https://api.themoviedb.org/3/authentication",{
+                headers
+            })
+        );
+    };
+};
