5fdb22cf33356497d3b8d3fbfff58ee2c9879209
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-03 17:37:17 -0300

    fixed: Now the Auth search for the api_key in the header

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -5,8 +5,9 @@ import { Observable } from "rxjs";
 export class AuthGuard implements CanActivate{
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         const response = context.switchToHttp().getRequest();
-        const api_key = response.headers;
-        if(api_key && api_key === String(process.env.API_KEY)){
+        const api_key = response.headers["api_key"];
+
+        if(api_key && api_key === process.env.API_KEY){
             return true;
         };
             return false;
