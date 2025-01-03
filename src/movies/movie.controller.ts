e809d62a3d0121efc03a05e7e4a2e35e5ee5c528
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-03 19:58:19 -0300

    feat: Get method to get the test data

--- src/movies/movie.controller.ts
+++ src/movies/movie.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Logger, Res, UseGuards } from "@nestjs/common";
+import { MovieService } from "./movie.service";
+import { Response } from "express";
 
-@Controller("movies")
-export class MovieController{};
+@Controller("movie")
+export class MovieController{
+    private readonly logger = new Logger(MovieController.name);
+    constructor(private readonly movieService:MovieService){};
+
+    @Get("/v1/test")
+    private async testAPI(@Res()res:Response) {
+        try{
+            const resultTest = await this.movieService.movieAPITest();
+            return res.status(200).send(resultTest.data);
+        }catch(err){
+            this.logger.error(`${err.message}`);
+            return res.status(500).json({server:`${err.message}`});
+        };
+    };
+};
