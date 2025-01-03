558059df9e5cf1a78cad3332414ee2d59e63257e
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-03 17:40:30 -0300

    fixed: Removed thE google auth for a moment

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { GoogleGuard } from './guards/auth.guards';
+import { GoogleGuard } from './guards/google.auth.guard';
 import { ConfigModule } from '@nestjs/config';
 import authGoogleConfig from './config/auth.google.config';
 import { PassportModule } from '@nestjs/passport';
