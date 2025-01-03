d9b1c5d47007fc7cee260a54e480d930126e2742
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-03 18:18:46 -0300

    feat: Created a worker do process the decode route

---
+++ src/auth/processor/auth.processor.ts
@@ -0,0 +1,14 @@
+import { Process, Processor } from "@nestjs/bull";
+import { AUTH_QUEUE } from "src/constants/constants";
+import { AuthController } from "../auth.controller";
+import { Job } from "bull";
+
+@Processor(AUTH_QUEUE)
+export class AuthProcessor{
+    constructor(private readonly authController:AuthController){};
+
+    @Process("docode")
+    private async decoedProcessToken(job:Job){
+        await this.authController.decodeToken(job.data);
+    };
+};
