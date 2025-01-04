22d52af69872d37d5182ce57e2005560bf518a7a
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-03 22:12:50 -0300

    feat: Registered a new queue

--- src/movies/movie.module.ts
+++ src/movies/movie.module.ts
@@ -2,9 +2,16 @@ import { HttpModule } from "@nestjs/axios";
 import { Module } from "@nestjs/common";
 import { MovieController } from "./movie.controller";
 import { MovieService } from "./movie.service";
+import { BullModule } from "@nestjs/bull";
+import { MOVIE_QUEUE } from "src/constants/constants";
 
 @Module({
-    imports:[HttpModule],
+    imports:[
+        HttpModule,
+        BullModule.registerQueue({
+            name:MOVIE_QUEUE
+        }),
+    ],
     controllers:[MovieController],
     providers:[MovieService]
 })
