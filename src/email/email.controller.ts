5ccba7a8a3085d0e3ece03a3128abb9f9b802bf0
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-08 13:41:26 -0300

    feat:New method to send the email in the controller

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Logger, Post, Res } from "@nestjs/common";
+import { EmailService } from "./email.service";
+import { SendEmail } from "./DTO/email.dto";
+import { Response } from "express";
 
 @Controller("email")
-export class EmailController{};
+export class EmailController{
+    private readonly logger = new Logger(EmailController.name);
+    constructor(private readonly emailService:EmailService){};
+
+    @Post("/v1/send")
+    private async sendOneEmail(@Body()data:SendEmail,@Res()res:Response):Promise<Response>{
+        try{
+            const email = await this.emailService.sendEmail(data);
+
+            return res.status(202).json({server:`Done`});
+
+        } catch(err){
+            this.logger.error(`${err.message}`);
+            return res.status(err.status).json({server:`${err.message}`});
+        };
+    };
+};
