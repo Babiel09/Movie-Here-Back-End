1db6528c445c1650b5b4262a1a74fc7bfe8c3235
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-08 13:27:33 -0300

    feat: Created a method to send the emails

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from "@nestjs/common";
+import { MailerService } from "@nestjs-modules/mailer";
+import { HttpException, Injectable, Logger } from "@nestjs/common";
+import { SendEmail } from "./DTO/email.dto";
 
 @Injectable()
-export class EmailService{};
+export class EmailService{
+    private readonly logger = new Logger(EmailService.name);
+    constructor(private readonly mailerService:MailerService){};
+
+    public async sendEmail(data:SendEmail):Promise<any>{
+        try{
+            const tryToSendEmail = await this.mailerService.sendMail({
+                to:data.to,
+                from:data.from,
+                subject:data.subject,
+                text:data.text,
+                html:data.html
+            });
+
+            return tryToSendEmail;
+        } catch(err){
+            this.logger.error(`${err.message}`);
+            throw new HttpException(`${err.message}`,500);
+        };
+    };
+};
