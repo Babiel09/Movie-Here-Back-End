05d7e6e625545a934430bb533d68baf55fe99b0f
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-08 18:21:29 -0300

    feat: Created a new constant to strore the email queue value

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,3 +2,4 @@ export const USER_QUEUE = "user-queue";
 export const GOOGLE_OAUTH = "google-oatuh";
 export const AUTH_QUEUE = "auth-queue"; 
 export const MOVIE_QUEUE = "movie-queue";
+export const EMAIL_QUEUE = "email-queue";
