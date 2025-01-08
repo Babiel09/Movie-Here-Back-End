411378eacc17054be5172b1244d3c64fbb984d40
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-08 13:12:28 -0300

    config: Configured the Mailer Module in the email module

--- src/email/emai.module.ts
+++ src/email/emai.module.ts
@@ -1,8 +1,20 @@
 import { Module } from "@nestjs/common";
 import { EmailController } from "./email.controller";
 import { EmailService } from "./email.service";
+import { MailerModule } from "@nestjs-modules/mailer";
 
 @Module({
+    imports:[
+        MailerModule.forRoot({
+            transport:{
+              host:"smtp.gmail.com",
+              auth:{
+                user:process.env.GMAIL_USER,
+                pass:process.env.GMAIL_PASS,
+              }
+            }
+          }),
+    ],
     controllers:[EmailController],
     providers:[EmailService],
     exports:[EmailService]
