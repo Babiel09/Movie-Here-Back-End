7048e23bf245e79405b7b4f6329bfd56012720ed
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-09 12:28:42 -0300

    feat: Make the sendEmail method send the same  email for the specified user

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -10,25 +10,54 @@ export class EmailService{
     private readonly logger = new Logger(EmailService.name);
     constructor(private readonly mailerService:MailerService,@InjectQueue(EMAIL_QUEUE)private readonly emailQueue:Queue){};
 
-    public async sendEmail(data:SendEmail):Promise<EmailService>{
+    public async sendEmail(email:string):Promise<EmailService>{
 
         try{
             const tryToSendEmail = await this.mailerService.sendMail({
-                to:data.to,
-                from:data.from,
-                subject:data.subject,
-                text:data.text,
-                html:data.html
+                to:email,
+                from:process.env.GMAIL_USER,
+                subject:"Verificar Conta",
+                text:"Sua conta está verificada agora você pode voltar para o site!",
+                html:`
+
+        <div style="background: linear-gradient(to right, #AD8CEA, #50DFB2); padding: 10px;">
+
+        <div style="background: linear-gradient(to right, #1A2980, #26DDCE); color: white; padding: 20px; text-align: center; font-family: Arial, sans-serif;">
+            <h1>Movie Here</h1>
+        </div>
+    
+        <div style="margin: 20px; padding: 20px; border: 2px solid blue; border-radius: 10px; display: flex; justify-content: center;">
+            <p style="font-size: 24px; color: white; font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;">
+                Sua conta está verificada agora você pode voltar para o site!
+            </p>
+            </div>
+        </div>
+    </div>
+                `
             });
 
             this.logger.debug("Working in a new job in the Email Queue");
             
             const emailJob = await this.emailQueue.add(EMAIL_QUEUE,{
-               jobTo:data.to,
-               jobFrom:data.from,
-               jobSubject:data.subject,
-               jobText:data.text,
-               jobHtml:data.html,
+               jobTo:email,
+               jobFrom:process.env.GMAIL_USER,
+               jobSubject:"Verificar Conta",
+               jobText:"Sua conta está verificada agora você pode voltar para o site!",
+               jobHtml:`
+        <div style="background: linear-gradient(to right, #AD8CEA, #50DFB2); padding: 10px;">
+
+        <div style="background: linear-gradient(to right, #1A2980, #26DDCE); color: white; padding: 20px; text-align: center; font-family: Arial, sans-serif;">
+            <h1>Movie Here</h1>
+        </div>
+    
+        <div style="margin: 20px; padding: 20px; border: 2px solid blue; border-radius: 10px; display: flex; justify-content: center;">
+            <p style="font-size: 24px; color: white; font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;">
+                Sua conta está verificada agora você pode voltar para o site!
+            </p>
+            </div>
+        </div>
+    </div>
+               `
             });
 
             this.logger.debug(`Processed job: ${JSON.stringify(emailJob.data)}`);
@@ -45,8 +74,8 @@ export class EmailService{
             const tryToSendEmailForTheUser  = await this.mailerService.sendMail({
                 to:userEmail,
                 from:process.env.GMAIL_USER,
-                subject:"Verify your account",
-                text:"Click in the button bellow to verify your account in our web site!",
+                subject:"Verificar sua Conta",
+                text:"Clique no botão abaixo para verificar sua conta em nosso website!",
                 html:`
 
       <div style="background: linear-gradient(to right, #AD8CEA, #50DFB2); padding: 10px;">
@@ -57,12 +86,12 @@ export class EmailService{
     
         <div style="margin: 20px; padding: 20px; border: 2px solid blue; border-radius: 10px; display: flex; justify-content: center;">
             <p style="font-size: 24px; color: white; font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;">
-                Click the button below to verify your account on our website!
+                Clique no botão abaixo para verificar sua conta em nosso website!
             </p>
             </div>
             <div style="text-align: center; margin-top: 20px;">
                 <a href="#" style="background: linear-gradient(to right, #6A11CB, #2575FC); color: white; padding: 10px 30px; text-decoration: none; border-radius: 10px; font-size: 16px;">
-                    Verify Account
+                    Verificar
                 </a>
             </div>
         </div>
