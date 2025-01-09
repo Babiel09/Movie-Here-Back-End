069fe5fcb92a9538fe135ecdd22737e8d278446c
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-09 11:45:59 -0300

    feat: new method to send a email telling the user to verify their account follow the link bellow

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -10,7 +10,7 @@ export class EmailService{
     private readonly logger = new Logger(EmailService.name);
     constructor(private readonly mailerService:MailerService,@InjectQueue(EMAIL_QUEUE)private readonly emailQueue:Queue){};
 
-    public async sendEmail(data:SendEmail):Promise<any>{
+    public async sendEmail(data:SendEmail):Promise<EmailService>{
 
         try{
             const tryToSendEmail = await this.mailerService.sendMail({
@@ -35,6 +35,43 @@ export class EmailService{
 
             return tryToSendEmail;
         } catch(err){
+            this.logger.error(`${err.message}`);
+            throw new HttpException(`${err.message}`,err.status);
+        };
+    };
+
+    public async sendEmailToVerify(userEmail:string):Promise<EmailService>{
+        try{
+            const tryToSendEmailForTheUser  = await this.mailerService.sendMail({
+                to:userEmail,
+                from:process.env.GMAIL_USER,
+                subject:"Verify your account",
+                text:"Click in the button bellow to verify your account in our web site!",
+                html:`
+
+      <div style="background: linear-gradient(to right, #AD8CEA, #50DFB2); padding: 10px;">
+
+        <div style="background: linear-gradient(to right, #1A2980, #26DDCE); color: white; padding: 20px; text-align: center; font-family: Arial, sans-serif;">
+            <h1>Movie Here</h1>
+        </div>
+    
+        <div style="margin: 20px; padding: 20px; border: 2px solid blue; border-radius: 10px; display: flex; justify-content: center;">
+            <p style="font-size: 24px; color: white; font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;">
+                Click the button below to verify your account on our website!
+            </p>
+            </div>
+            <div style="text-align: center; margin-top: 20px;">
+                <a href="#" style="background: linear-gradient(to right, #6A11CB, #2575FC); color: white; padding: 10px 30px; text-decoration: none; border-radius: 10px; font-size: 16px;">
+                    Verify Account
+                </a>
+            </div>
+        </div>
+    </div>
+                `
+          });
+
+            return tryToSendEmailForTheUser;;
+        }catch(err){
             this.logger.error(`${err.message}`);
             throw new HttpException(`${err.message}`,err.status);
         };
