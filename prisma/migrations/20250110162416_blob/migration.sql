3274b7ead74240bebb3f33c89773f07b98305a4c
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-10 13:24:54 -0300

    config: New migrations

--- prisma/migrations/20250110162416_blob/migration.sql
+++ prisma/migrations/20250110162416_blob/migration.sql
@@ -7,21 +7,13 @@ CREATE TABLE "User" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "photo" TEXT NOT NULL DEFAULT '',
+    "photo" BYTEA NOT NULL DEFAULT '\x',
     "description" TEXT NOT NULL DEFAULT '',
     "verified" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "role" "Roles" NOT NULL DEFAULT 'User',
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Movies" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "Movies_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
