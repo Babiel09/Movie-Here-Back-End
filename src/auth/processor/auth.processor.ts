2398e2a84ad73775e27088245728222f61cece30
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-12 14:55:09 -0300

    feat New Worker to process the new job

--- src/auth/processor/auth.processor.ts
+++ src/auth/processor/auth.processor.ts
@@ -2,10 +2,11 @@ import { Process, Processor } from "@nestjs/bull";
 import { AUTH_QUEUE } from "src/constants/constants";
 import { AuthController } from "../auth.controller";
 import { Job } from "bull";
+import { GoogleStrategy } from "../google/auth.google.strategy";
 
 @Processor(AUTH_QUEUE)
 export class AuthProcessor{
-    constructor(private readonly authController:AuthController){};
+    constructor(private readonly authController:AuthController,private readonly googleStrategy:GoogleStrategy){};
 
     @Process("docode")
     private async decoedProcessToken(job:Job){
@@ -16,4 +17,9 @@ export class AuthProcessor{
     private async showOAuthGoogleTokne(job:Job){
         await this.authController.googleCalback(job.data.res,job.data.req);
     };
+
+    @Process("new_user")
+    private async googleStrategyWorker(){
+        await this.googleStrategy.validate();
+    }
 };
