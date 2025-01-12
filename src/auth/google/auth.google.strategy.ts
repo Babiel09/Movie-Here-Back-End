97e72a687aa566ee81e9f38909330fc55e56b1a6
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-12 14:09:14 -0300

    feat: All works to create a new user with the google oauth

--- src/auth/google/auth.google.strategy.ts
+++ src/auth/google/auth.google.strategy.ts
@@ -1,11 +1,13 @@
 import { Injectable, Logger } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy,VerifyCallback } from "passport-google-oauth20";
+import { UserService } from "src/user/user.service";
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, "google"){
     private readonly logger = new Logger(GoogleStrategy.name);
-    constructor(){
+    constructor(private readonly jwtService:JwtService,private readonly userService:UserService){
         super({
             clientID:process.env.GOOGLE_CLIENT_ID,
             clientSecret:process.env.GOOGLE_CLIENT_SECRET,
@@ -22,10 +24,27 @@ export class GoogleStrategy extends PassportStrategy(Strategy, "google"){
             name:name.givenName,
             email: emails[0].value,
             photo:photos[0].value,
-            accessToken
-        }
-        
+            token:accessToken
+        };
+
+        const photoArrayBuffer = new ArrayBuffer(userWithGoogle.photo);
+
+        const bytes = new Uint8Array(photoArrayBuffer);
+
         this.logger.debug(userWithGoogle);
+
+        userWithGoogle.photo = bytes;
+
+        const newUserWithGoogle = await this.userService.InsertuserWIthGoogle(userWithGoogle)
+
+        const payload = {id:newUserWithGoogle.id,name:newUserWithGoogle.name,email:newUserWithGoogle.email,password:newUserWithGoogle.password,description:newUserWithGoogle.description}
+
+        const token = await this.jwtService.signAsync(payload);
+
+        this.logger.debug(token);
+
+        return token;
+        
     };
 
 };
