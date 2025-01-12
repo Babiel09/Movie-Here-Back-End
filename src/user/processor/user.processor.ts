a9832db4ea410bafaf2dbbe2ee51e19f23dd4f57
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-12 15:03:51 -0300

    config: Instantiating user controller in the user processor

--- src/user/processor/user.processor.ts
+++ src/user/processor/user.processor.ts
@@ -2,10 +2,11 @@ import { Process, Processor } from "@nestjs/bull";
 import { USER_QUEUE } from "src/constants/constants";
 import { UserService } from "../user.service";
 import { Job } from "bull";
+import { UserController } from "../user.controller";
 
 @Processor(USER_QUEUE)
 export class UserProcessor{
-    constructor(private readonly userService:UserService){};
+    constructor(private readonly userService:UserService,private readonly userController:UserController){};
 
     @Process("login")
     private async processLogin(job:Job){
