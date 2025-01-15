f6b6a22b1146938df67f3f52972685ce800e1914
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-15 15:19:31 -0300

    feat: New method to change the user logger with the Google OAuth2.0

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, Injectable, Logger } from "@nestjs/common";
+import { Prisma, User } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
+import { PrismaService } from "prisma/prisma.service";
 
 @Injectable()
-export class AuthService{};
+export class AuthService{
+    private readonly logger = new Logger(AuthService.name);
+    private readonly prisma: Prisma.UserDelegate<DefaultArgs>;
+    constructor(private readonly pr:PrismaService){
+        this.prisma = pr.user;
+    };
+
+    public async changeUserWithGooglePhoto(id:number,newPassword:string):Promise<User>{
+        try{
+            const findUser = await this.prisma.findUnique({
+                where:{
+                    id:id
+                }
+            });
+
+            if(!findUser){
+                this.logger.error(`We can't find the user with this id: ${id}`);
+                throw new HttpException(`We can't find the user with this id: ${id}`,401);
+            };
+
+            const updateUserWithGooglePass = await this.prisma.update({
+                where:{
+                    id:findUser.id,
+                },
+                data:{
+                    password:newPassword,
+                },
+            });
+
+            return updateUserWithGooglePass;
+
+        } catch(err){
+            this.logger.error(`${err.message}`);
+            throw new HttpException(`${err.message}`,err.status);
+        };
+    };
+};
