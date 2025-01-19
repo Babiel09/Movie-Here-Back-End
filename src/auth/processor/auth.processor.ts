9129f4663469f5e1eeda99faeea2f20a940eafed
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-19 18:15:41 -0300

    feat: New worker to process the method to create a new password to the user logger with the google oauth2.0

--- src/auth/processor/auth.processor.ts
+++ src/auth/processor/auth.processor.ts
@@ -3,10 +3,11 @@ import { AUTH_QUEUE } from "src/constants/constants";
 import { AuthController } from "../auth.controller";
 import { Job } from "bull";
 import { GoogleStrategy } from "../google/auth.google.strategy";
+import { AuthService } from "../auth.service";
 
 @Processor(AUTH_QUEUE)
 export class AuthProcessor{
-    constructor(private readonly authController:AuthController,private readonly googleStrategy:GoogleStrategy){};
+    constructor(private readonly authController:AuthController,private readonly googleStrategy:GoogleStrategy,private readonly authService:AuthService){};
 
     @Process("docode")
     private async decoedProcessToken(job:Job){
@@ -16,5 +17,10 @@ export class AuthProcessor{
     @Process("new_user")
     private async googleStrategyWorker(){
         await this.googleStrategy.validate();
-    }
+    };
+
+    @Process("new_password_google_aouth20")
+    private async googleCreatePassword(job:Job){
+        await this.authService.changeUserWithGooglePassword(job.data.id,job.data.newPassword);
+    };
 };
