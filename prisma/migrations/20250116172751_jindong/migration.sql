90b1f3a92d888b6b9e0000151b77895a09cf2a8f
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-20 12:47:55 -0300

    config: new migration

--- prisma/migrations/20250116172751_jindong/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[realId]` on the table `Movies` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Movies_realId_key" ON "Movies"("realId");
