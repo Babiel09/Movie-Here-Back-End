beecfdcffb220e05e63938593a87b837c00c5ca5
Gabriel Castro <estudosbabiel@gmail.com>
2025-01-20 08:09:53 -0300

    feat: Added the userId and the moveId to the DTO to rate the movie

--- src/movies/DTO/movie.rate.dto.ts
+++ src/movies/DTO/movie.rate.dto.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { IsNumber } from 'class-validator';
+import { IsInt, IsNumber } from 'class-validator';
 
 @Injectable()
 export class VoteMovieDTO{
+
+  @IsNumber(undefined,{message:"The movie id needs to be a number!"})
+  @IsInt({message:"The movie id needs to be a int value!"})
+  movieId:number;
+
+  @IsNumber(undefined,{message:"The user id needs to be a number!"})
+  @IsInt({message:"The user id needs to be a int value!"})
+  userId:number;
+
   @IsNumber(undefined,{message:"The vote needs to be a number!"})
   vote:number;
 };
