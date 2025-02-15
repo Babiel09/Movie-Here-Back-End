5fb805b80768116835eda58c968d1f3ee0eded6e
Gabriel Castro <estudosbabiel@gmail.com>
2025-02-15 16:38:04 -0300

    feat: Created a method to hash some value

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,5 +3,10 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AppService{
-    
+    public async hashRandomSalt(valueToHash:string){
+        const saltArray:number[] = [12,14,16,18,20,22,24,26,28,30,32,34,36,38,40];
+        for(let i = 0; i < saltArray.length; i++){
+          await bcrypt.hash(valueToHash,saltArray[i])
+          };
+        };
 };
