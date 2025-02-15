3bbf293784cfe8c988d69e52b8d776218a50dfd0
Gabriel Castro <estudosbabiel@gmail.com>
2025-02-15 16:07:28 -0300

    feat: New log when the Redis is on or when the Redis is off

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import Redis from 'ioredis';
 
 @Injectable()
 export class RedisService extends Redis{
+    private readonly logger = new Logger(RedisService.name);
     constructor(){
         super();
+        if(super.on){   
+            this.logger.log("Redis ON!");
+        }else{
+            this.logger.error("Redis OFF!");
+        };
     };
 };
