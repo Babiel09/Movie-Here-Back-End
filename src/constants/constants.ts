32bdabf3faac6669641c718246d16756b66d52fe
Gabriel Castro <estudosbabiel@gmail.com>
2025-02-15 17:10:54 -0300

    deleted: Deleted the unnecessary variable

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,4 +3,3 @@ export const GOOGLE_OAUTH = "google-oatuh";
 export const AUTH_QUEUE = "auth-queue"; 
 export const MOVIE_QUEUE = "movie-queue";
 export const EMAIL_QUEUE = "email-queue";
-export const USER_CACHE_KEY = "user";
