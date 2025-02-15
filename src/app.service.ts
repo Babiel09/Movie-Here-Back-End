bf45f7cd0a2eeabeeb65744acc5657b21bcdf8cd
Gabriel Castro <estudosbabiel@gmail.com>
2025-02-15 16:53:37 -0300

    refactored: When i do the hash, the program will chosse an random number into the linked list to use it like a salt

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,8 +5,8 @@ import * as bcrypt from 'bcrypt';
 export class AppService{
     public async hashRandomSalt(valueToHash:string){
         const saltArray:number[] = [12,14,16,18,20,22,24,26,28,30,32,34,36,38,40];
-        for(let i = 0; i < saltArray.length; i++){
-          await bcrypt.hash(valueToHash,saltArray[i])
-          };
-        };
+        let salt:number = Math.floor(Math.random() * saltArray.length);
+        const newValue = await bcrypt.hash(valueToHash,salt)
+        return newValue;
+   };
 };
