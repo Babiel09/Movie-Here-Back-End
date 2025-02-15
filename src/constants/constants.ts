e4c258e484b59bd6d68009f091ba3966c1494582
Gabriel Castro <estudosbabiel@gmail.com>
2025-02-15 15:51:09 -0300

    config: New constant to store the user cache key value

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,3 +3,4 @@ export const GOOGLE_OAUTH = "google-oatuh";
 export const AUTH_QUEUE = "auth-queue"; 
 export const MOVIE_QUEUE = "movie-queue";
 export const EMAIL_QUEUE = "email-queue";
+export const USER_CACHE_KEY = "user";
